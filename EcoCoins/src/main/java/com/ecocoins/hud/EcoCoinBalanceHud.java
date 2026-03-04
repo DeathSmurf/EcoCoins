2f5db85ed80d60e36ec6e01622b2672c48b6eae2
DeathSmurf <63019055+DeathSmurf@users.noreply.github.com>
2026-03-04 00:40:51 -0300

    fix: avoid re-registering HUD on each balance update

---
+++ EcoCoins/src/main/java/com/ecocoins/hud/EcoCoinBalanceHud.java
@@ -0,0 +1,38 @@
+package com.ecocoins.hud;
+
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+
+public final class EcoCoinBalanceHud extends SimpleHud {
+
+    public enum Position {
+        BOTTOM_LEFT,
+        BOTTOM_RIGHT;
+
+        public Position toggle() {
+            return this == BOTTOM_RIGHT ? BOTTOM_LEFT : BOTTOM_RIGHT;
+        }
+    }
+
+    private final Position position;
+
+    public EcoCoinBalanceHud(PlayerRef playerRef, Position position) {
+        super(playerRef, uiPathFor(position));
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void update(double balance) {
+        String value = String.format("%.2f", balance);
+        setText("BalanceAmount", value);
+        pushUpdates();
+    }
+
+    private static String uiPathFor(Position position) {
+        return position == Position.BOTTOM_LEFT
+                ? "Pages/EcoCoins_BalanceHud_Left.ui"
+                : "Pages/EcoCoins_BalanceHud_Right.ui";
+    }
+}
